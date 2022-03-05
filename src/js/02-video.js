e93784fdcd05ef45019b337569ca0fe4307befa7
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2022-03-05 20:46:14 +0200

    Написал скрипт для сохранения текущего времени в  localstorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 import '../css/common.css';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = "videoplayer-current-time";
+const ifraime = document.querySelector('iframe');
+const player = new  Vimeo.Player(ifraime);
+
+const onPlay = function(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds)
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function(seconds) {
+    localStorage.setItem(STORAGE_KEY, seconds)
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+    
+})
+
